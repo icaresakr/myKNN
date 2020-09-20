ef418321749c7f6845956b661037397ad32ddc88
icaresakr <icare.sakr@ensta-paris.fr>
2020-09-20 19:46:15 +0300

    Latest code

--- myKNN.py
+++ myKNN.py
@@ -1,8 +1,11 @@
 from math import *
 import numpy as np
-import csv
+from csv import reader
 import random
 import operator
+#sfrom sklearn import datasets, neighbors
+import matplotlib.pyplot as plt
+#from mlxtend.plotting import plot_decision_regions
 
 
 #To test with data for now
@@ -96,15 +99,138 @@ class myKNN:
 
         return confM
 
-###TESTING with random data
-Xtrain = [[1,1,1],[2,2,2],[1,3,6]]
-Ytrain = ['a','a','b']
-Predictor = myKNN(Xtrain,Ytrain) #train the KNN
+### Other Methods
+def load_csv(filename):
+	dataset = list()
+	Ylabels = list()
+	forbidden ='?'
+	with open(filename, 'r') as file:
+		csv_reader = reader(file)
+		for row in csv_reader:
+		    if not row:
+		        continue
+		    if forbidden not in row: #remove data containing a '?'
+		        dataset.append([int(row[i]) for i in range(1,len(row)-1)]) #without the labels
+		        Ylabels.append(int(row[-1])) #the labels
+	return (dataset,Ylabels)
 
-res = Predictor.predict([[3,3,3],[1,1,1],[3,2,5],[3,4,5]],1) #predict test data
-Ytest = ['a','a','b','a'] #actual labels
+def split_data(X,Y,probability):
+    X1 = list()
+    X2 = list()
+    Y1 = list()
+    Y2 = list()
+    #X and Y should have same dimension
+    for i in range(len(X)):
+        j = random.random()
+        if j<=probability:
+            X1.append(X[i])
+            Y1.append(Y[i])
+        else:
+            X2.append(X[i])
+            Y2.append(Y[i])
+    return(X1,Y1,X2,Y2)
 
+def dataset_minmax(dataset):
+	minmax = list()
+	for i in range(len(dataset[0])):
+		col_values = [row[i] for row in dataset]
+		value_min = min(col_values)
+		value_max = max(col_values)
+		minmax.append([value_min, value_max])
+	return minmax
+
+
+def normalize_data(X):
+    minmax = dataset_minmax(X)
+    for row in X:
+        for i in range(len(row)):
+            row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+
+#plot for 2 classes
+def plot_data(dim1, dim2, X, Y):
+    for i in range(len(X)):
+        if(Y[i]==Y[0]):
+            plt.plot(X[i][dim1], X[i][dim2],'bs') #in blue square for class 1
+        else:
+            plt.plot(X[i][dim1], X[i][dim2],'g^') #in green triangle for class 2
+    plt.show()
+
+### First data set
+Xall, Yall = load_csv("./breast-cancer-wisconsin.data")
+
+#normalize_data(Xall)
+
+
+Xtrain,Ytrain,Xtest,Ytest = split_data(Xall,Yall,0.8) # split data 80% train, 20% test
+
+Predictor = myKNN(Xtrain,Ytrain)
+
+##CHECK OPTIMAL VALUE FOR K
+'''
+#for plotting error as a function of K
+error  = []
+Ks = []
+
+for k in range(1,20):
+    res = Predictor.predict(Xtest,k) #predict test data
+
+    #print("Confusion Matrix:\n" + str(Predictor.getConfusionMatrix(Ytest,res))+"\n")
+    error.append(100 - Predictor.getAccuracy(Ytest,res))
+    Ks.append(k)
+
+
+plt.plot(Ks, error)
+plt.xlabel('K value')
+plt.ylabel('Error (%)')
+
+plt.title('Test prediciton Error as a function of K')
+
+plt.show()
+'''
+
+
+#K = 2 IS OPTIMAL FOR THIS DATA SET (or Ks[argmin(error)])
+
+##BEFORE NORMALIZING DATA
+res = Predictor.predict(Xtest,2) #predict test data
+
+#Accuracy
+print("Accuracy:\n" + str(Predictor.getAccuracy(Ytest,res))+"\n")
+
+#Confusion matrix
 print("Confusion Matrix:\n" + str(Predictor.getConfusionMatrix(Ytest,res))+"\n")
-print("Accuracy:\n" + str(Predictor.getAccuracy(Ytest,res)) + "%"+"\n")
+
+#NORMALIZE DATA
+#normalize_data(Xtrain)
+#normalize_data(Xtest)
+
+##AFTER NORMALIZING DATA
+'''
+Predictor.train(Xtrain,Ytrain)
+
+res2 = Predictor.predict(Xtest,2)
+
+#Accuracy
+print("Accuracy:\n" + str(Predictor.getAccuracy(Ytest,res2))+"\n")
+
+#Confusion matrix
+print("Confusion Matrix:\n" + str(Predictor.getConfusionMatrix(Ytest,res2))+"\n")
+
+'''
+#no difference because all the variables have the same range of variation
+
+##
+
+
+
+#plot the data
+plot_data(6,7,Xall,Yall)
+
+# Adding axes annotations
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title("Knn with K="+ str(2))
+plt.show()
+
 
 
